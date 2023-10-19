578ed56cc6806e2e7f4ec0490451ca50ee2baff4
cafalchio <mcafalchio@gmail.com>
2023-10-19 22:55:54 +0100

    feat: created basic animations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,103 +1,110 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const CANVAS_W = (canvas.width = 1600);
-const CANVAS_H = (canvas.height = 800);
-let gameSpeed = 1;
-let gameFrame = 0;
 
-class Layer {
-  constructor(image, speedModifier) {
-    this.x = 0;
-    this.y = 0;
-    this.width = CANVAS_W; 
-    this.height = CANVAS_H;
-    this.image = image;
-    this.speedModifier = speedModifier;
-    this.speed = gameSpeed * this.speedModifier;
+
+function handleResize() {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const newWidth = window.innerWidth;
+  const newHeight = window.innerHeight;
+
+  const CANVAS_W = (canvas.width = newWidth - newWidth * .1);
+  const CANVAS_H = (canvas.height = newHeight - newHeight * .1);
+
+  let gameSpeed = 1;
+  let gameFrame = 0;
+
+  class Layer {
+    constructor(image, speedModifier) {
+      this.x = 0;
+      this.y = 0;
+      this.width = CANVAS_W; 
+      this.height = CANVAS_H;
+      this.image = image;
+      this.speedModifier = speedModifier;
+      this.speed = gameSpeed * this.speedModifier;
+    }
+
+    update() {
+      this.speed = gameSpeed * this.speedModifier;
+      this.x = gameFrame * this.speed % - this.width;
+    }
+
+    draw() {
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+      ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+    }
   }
 
-  update() {
-    this.speed = gameSpeed * this.speedModifier;
-    this.x = gameFrame * this.speed % - this.width;
+  // Create the yard Background
+  const yardBackground = [
+    { img: 'assets/images/background/0_sky.png', speed: 0.0001 },
+    { img: 'assets/images/background/1_moon.png', speed: 0.0002 },
+    { img: 'assets/images/background/2_landscape.png', speed: 0.002 },
+    { img: 'assets/images/background/3_building.png', speed: 0.05 },
+    { img: 'assets/images/background/4_mist_b.png', speed: 0.1 },
+    { img: 'assets/images/background/5_yard.png', speed: 0.2 },
+    { img: 'assets/images/background/6_mist_a.png', speed: 0.3 },
+    { img: 'assets/images/background/7_road.png', speed: 0.5 },
+    { img: 'assets/images/background/road_stone.png', speed: 1 },
+  ];
+
+  function createBackground(images) {
+    const backgrounds = images.map(img => {
+      const back = new Image();
+      back.src = img.img;
+      return new Layer(back, img.speed);
+    });
+    return backgrounds;
   }
 
-  draw() {
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+  const background = createBackground(yardBackground);
+
+  // end background
+
+  const enemyImage = new Image();
+  enemyImage.src = 'assets/images/monsters/ghosts.png';
+
+
+  class Enemy {
+    constructor() {
+      this.x = 200;
+      this.y = 100;
+      this.speed = Math.random() ;
+      this.spriteW = 158;
+      this.spriteH = 152;
+      this.width = this.spriteW  / 2;
+      this.height = this.spriteH / 2;
+      this.frame = 1;
+    }
+    update(){
+      this.x += Math.sin(this.speed + 2);
+      this.y += this.speed;
+      if (gameFrame % 8 == 0) this.frame >= 4 ? this.frame = 0 : this.frame++;
+    }
+    draw() {
+      ctx.drawImage(enemyImage, this.frame * this.spriteW, 0, this.spriteW, this.spriteH, this.x, this.y, this.width, this.height)
+    }
   }
+  const enemiesArray = [];
+
+  for (let i = 0; i< 10; i++) {
+    enemiesArray.push(new Enemy());
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_W, CANVAS_H);
+    background.forEach(back => {
+      back.draw();
+      back.update();
+    });
+    enemiesArray.forEach(enemy => {
+      enemy.draw();
+      enemy.update();
+    });
+    gameFrame--;
+    requestAnimationFrame(animate);
+  }
+  animate();
 }
+handleResize()
+window.addEventListener("resize", handleResize);
 
-// Create the yard Background
-const yardBackground = [
-  { img: 'assets/images/background/0_sky.png', speed: 0.0001 },
-  { img: 'assets/images/background/1_moon.png', speed: 0.0002 },
-  { img: 'assets/images/background/2_landscape.png', speed: 0.002 },
-  { img: 'assets/images/background/3_building.png', speed: 0.05 },
-  { img: 'assets/images/background/4_mist_b.png', speed: 0.1 },
-  { img: 'assets/images/background/5_yard.png', speed: 0.2 },
-  { img: 'assets/images/background/6_mist_a.png', speed: 0.3 },
-  { img: 'assets/images/background/7_road.png', speed: 0.5 },
-  { img: 'assets/images/background/road_stone.png', speed: 1 },
-];
-
-function createBackground(images) {
-  const backgrounds = images.map(img => {
-    const back = new Image();
-    back.src = img.img;
-    return new Layer(back, img.speed);
-  });
-  return backgrounds;
-}
-
-const background = createBackground(yardBackground);
-
-// end background
-
-// Enemies
-
-const enemyImage = new Image();
-enemyImage.src = 'assets/images/monsters/ghosts.png';
-
-
-class Enemy {
-  constructor() {
-    this.x = 200;
-    this.y = 100;
-    this.speed = Math.random() ;
-    this.spriteW = 158;
-    this.spriteH = 152;
-    this.width = this.spriteW  / 2;
-    this.height = this.spriteH / 2;
-    this.frame = 0;
-  }
-  update(){
-    this.x += Math.sin(this.speed + 2);
-    this.y += this.speed;
-    if (gameFrame % 8 == 0) this.frame >= 3 ? this.frame = 0 : this.frame++;
-  }
-  draw() {
-    ctx.drawImage(enemyImage, this.frame * this.spriteW, 0, this.spriteW, this.spriteH, this.x, this.y, this.width, this.height)
-  }
-}
-const enemiesArray = [];
-
-for (let i = 0; i< 10; i++) {
-  enemiesArray.push(new Enemy());
-}
-
-
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_W, CANVAS_H);
-  background.forEach(back => {
-    back.draw();
-    back.update();
-  });
-  enemiesArray.forEach(enemy => {
-    enemy.draw();
-    enemy.update();
-  });
-  gameFrame--;
-  requestAnimationFrame(animate);
-}
-
-animate();
