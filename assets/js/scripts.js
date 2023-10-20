a5a18b5cb3c424dd430bd72c3648f6cd04ceb1f6
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-10-20 20:05:53 +0200

    feat: add js file for index.html

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -170,21 +170,3 @@ handleResize()
 // look for resizes on the screen
 window.addEventListener("resize", handleResize);
 
-// Mute and mute index page
-const audio = document.querySelector('audio');
-const volumeButton = document.getElementById('volumeButton');
-const volumeIcon = document.getElementById('volumeIcon');
-
-function toggleAudio() {
-    if (audio.paused) {
-        audio.play();
-        volumeIcon.classList.remove('fa-volume-mute');
-        volumeIcon.classList.add('fa-volume-high');
-    } else {
-        audio.pause();
-        volumeIcon.classList.remove('fa-volume-high');
-        volumeIcon.classList.add('fa-volume-mute');
-    }
-}
-
-volumeButton.addEventListener('click', toggleAudio);
