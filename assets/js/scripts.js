8171b22b45e43a89dc8d215c86538aa7117a9251
Omar-Alme <90763798+Omar-Alme@users.noreply.github.com>
2023-10-20 19:35:55 +0200

    fix: move mute/unmute function to the bottom

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,21 +1,3 @@
-const audio = document.querySelector('audio');
-const volumeButton = document.getElementById('volumeButton');
-const volumeIcon = document.getElementById('volumeIcon');
-
-function toggleAudio() {
-    if (audio.paused) {
-        audio.play();
-        volumeIcon.classList.remove('fa-volume-mute');
-        volumeIcon.classList.add('fa-volume-high');
-    } else {
-        audio.pause();
-        volumeIcon.classList.remove('fa-volume-high');
-        volumeIcon.classList.add('fa-volume-mute');
-    }
-}
-
-volumeButton.addEventListener('click', toggleAudio);
-
 function handleResize() {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
@@ -181,3 +163,21 @@ handleResize()
 
 window.addEventListener("resize", handleResize);
 
+// Mute and mute index page
+const audio = document.querySelector('audio');
+const volumeButton = document.getElementById('volumeButton');
+const volumeIcon = document.getElementById('volumeIcon');
+
+function toggleAudio() {
+    if (audio.paused) {
+        audio.play();
+        volumeIcon.classList.remove('fa-volume-mute');
+        volumeIcon.classList.add('fa-volume-high');
+    } else {
+        audio.pause();
+        volumeIcon.classList.remove('fa-volume-high');
+        volumeIcon.classList.add('fa-volume-mute');
+    }
+}
+
+volumeButton.addEventListener('click', toggleAudio);
